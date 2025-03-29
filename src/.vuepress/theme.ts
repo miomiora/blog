00873994361763693805b38771b81ff8707c1342
miomiora <1729089090@qq.com>
2025-03-29 18:02:59 +0800

    update

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -23,7 +23,7 @@ export default hopeTheme({
 
   iconAssets: "//at.alicdn.com/t/c/font_3918030_kkg9un8oqxs.css",
 
-  logo: "https://s2.loli.net/2023/04/09/D5VmfW7otSsgHUh.png",
+  logo: "https://s2.loli.net/2025/03/29/Y6HKPFEzaSuOhJy.png",
 
   repo: "miomiora",
 
